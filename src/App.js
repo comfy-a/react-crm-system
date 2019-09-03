26d1147fbb22e9d8709369d11720388437f80124
comfyahn <comfyahn@gmail.com>
2019-09-03 17:49:49 +0900

    props를 이용하여 Customer 출력

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Customer from './components/Customer';
 
+const customer = {
+  'name': '홍길동',
+  'age': 34,
+  'birthday': '860305',
+  'gender': '남자'
+}
+
 function App() {
   return (
-    <Customer />
+    <Customer customer={customer} />
   );
 }
 
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -4,9 +4,10 @@ class Customer extends React.Component {
     render() {
         return(
             <div>
-                <h1>홍길동</h1>
-                <p>860305</p>
-                <p>남자</p>
+                <h1>{this.props.customer.name}</h1>
+                <p>{this.props.customer.age}</p>
+                <p>{this.props.customer.birthday}</p>
+                <p>{this.props.customer.gender}</p>                
             </div>
         )
     }
