5eb5bd37ad9e422c465bd48d327a8965ea2a129b
comfyahn <comfyahn@gmail.com>
2019-09-03 17:45:04 +0900

    Customer component 추가

---
+++ src/components/Customer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Customer extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>홍길동</h1>
+                <p>860305</p>
+                <p>남자</p>
+            </div>
+        )
+    }
+}
+
+export default Customer;
